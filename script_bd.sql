76736d902620dd9f551492473fcd8ced4c9363aa
Marcossakaguchi5 <jorge.sakaguchi@outlook.com>
2022-12-06 18:59:14 -0300

    banco de dados

--- script_bd.sql
+++ script_bd.sql
@@ -4,11 +4,12 @@ USE  fatecEventos;
 
 CREATE TABLE usuario(
 id_login int AUTO_INCREMENT PRIMARY KEY UNIQUE,
-login varchar(30) ,  
-senha varchar(30),
-perfil varchar(30),
-email varchar(100),
-telefone varchar(15)
+login varchar(30) ,
+email varchar(100),  
+senha varchar(700),
+telefone varchar(15),
+perfil varchar(30)
+
 );
 CREATE TABLE empresa(
 id_empresa int AUTO_INCREMENT PRIMARY KEY UNIQUE,
@@ -29,7 +30,9 @@ id_convidados int AUTO_INCREMENT PRIMARY KEY UNIQUE,
 nome varchar(100),
 email varchar(100),
 cargo varchar(100),
-telefone varchar(15)
+telefone varchar(15),
+id_empresa int,
+FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
 );
 
 CREATE TABLE  evento_convidados(
@@ -44,10 +47,11 @@ FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
 FOREIGN KEY (id_convidados) REFERENCES convidados(id_convidados)
 
 );
+
 use fatecEventos;
-INSERT INTO usuario (login,senha,perfil,email,telefone) VALUES ("nivea","nivea","adm","nivea@fsnt.com","1456859632");
-INSERT INTO usuario (login,senha,perfil,email,telefone) VALUES ("elvis","elvis","adm","elvis@fsnt.com","1485963256");
-INSERT INTO usuario (login,senha,perfil,email,telefone) VALUES ("carla","carla","usuario","carla@jacto.com","1425365689");
+INSERT INTO usuario (login,email,senha,telefone,perfil) VALUES ("nivea","nivea@fsnt.com","nivea","1456859632","adm");
+INSERT INTO usuario (login,email,senha,telefone,perfil) VALUES ("elvis","elvis@fsnt.com","elvis","1485963256","adm");
+INSERT INTO usuario (login,email,senha,telefone,perfil) VALUES ("carla","carla@jacto.com","carla","1425365689","usuario");
 SELECT * FROM usuario;
 
 INSERT INTO empresa (empresa) VALUES ("Ciag");
@@ -57,19 +61,19 @@ INSERT INTO empresa (empresa) VALUES ("Bruden");
 SELECT * FROM empresa;
 
 INSERT INTO evento (data_hora,descricao,id_login) 
-VALUES('2022-07-27 05:03:13','formatura bdag 2022/2','1');
+VALUES('2022-07-27','formatura bdag 2022/2','1');
 INSERT INTO evento (data_hora,descricao,id_login) 
-VALUES('2023-01-23 08:03:13','Formaturha map 2022/2','2');
+VALUES('2023-01-23','Formaturha map 2022/2','2');
 INSERT INTO evento (data_hora,descricao,id_login) 
-VALUES('2024-02-25 05:03:13','Formaturha bdag 2023/1','1');
+VALUES('2024-02-25','Formaturha bdag 2023/1','1');
 SELECT * FROM evento;
 
-INSERT INTO convidados (nome,email,cargo,telefone)
-VALUES ('Marcos Hideki','marcosHideki@Ciag.com','PesquisadorIII','14958362518');
-INSERT INTO convidados (nome,email,cargo,telefone)
-VALUES ('Giovana ','Giovana@jacto.com','Designer pleno','1536259876');
-INSERT INTO convidados (nome,email,cargo,telefone)
-VALUES ('Lucas Cabrera ','lucasCabrera@jacto.com','Front-End Pleno','1736259856');
+INSERT INTO convidados (nome,email,cargo,telefone,id_empresa)
+VALUES ('Marcos Hideki','marcosHideki@Ciag.com','PesquisadorIII','14958362518',1);
+INSERT INTO convidados (nome,email,cargo,telefone,id_empresa)
+VALUES ('Giovana ','Giovana@jacto.com','Designer pleno','1536259876',1);
+INSERT INTO convidados (nome,email,cargo,telefone,id_empresa)
+VALUES ('Lucas Cabrera ','lucasCabrera@jacto.com','Front-End Pleno','1736259856',1);
 SELECT * FROM convidados;
 
 INSERT INTO evento_convidados (id_evento,id_convidados,condicao,anunciados,presenca) 
@@ -81,4 +85,5 @@ VALUES (1,2,"nao respondeu",0,0);
 INSERT INTO evento_convidados (id_evento,id_convidados,condicao,anunciados,presenca) 
 VALUES (1,3,"nao respondeu",0,0);
 SELECT * FROM evento_convidados;
-docker run --name mysql2 -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql
+
+-- docker run --name mysql2 -e MYSQL_ROOT_PASSWORD=root -p 3306:3306 -d mysql
